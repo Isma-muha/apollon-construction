31f9ad1a0a97479bf8a9d21c9d5169cd8985da67
Isma-muha <isma-muha@hotmail.com>
2026-04-18 09:49:51 +0200

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -39,7 +39,7 @@ const ContactPage: NextPage = () => {
     setIsSubmitting(true)
     const { _honeypot, ...payload } = data
     try {
-      const res = await fetch('https://formspree.io/f/mvzvdenn', {
+      const res = await '/api/lead'
         method: 'POST',
         headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
         body: JSON.stringify(payload),
